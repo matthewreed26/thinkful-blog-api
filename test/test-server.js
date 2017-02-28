be057effa83bb09509f69696c6ce6c12589b26b5
Matthew Reed <matthewreed26@gmail.com>
2017-02-27 22:54:26 -0500

    have mongoose use global promises

--- test/test-server.js
+++ test/test-server.js
@@ -2,6 +2,7 @@ const chai = require('chai');
 const chaiHttp = require('chai-http');
 const faker = require('faker');
 const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
 
 const {Post} = require('../models');
 const {app, runServer, closeServer} = require('../server');
